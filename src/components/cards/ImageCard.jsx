f850492edd11c17ac785b7ce48bc4588d2b7915b
Varu <vardaan.agarwal.98@gmail.com>
2023-06-30 13:10:23 +0530

    chore : remove comments

--- src/components/cards/ImageCard.jsx
+++ src/components/cards/ImageCard.jsx
@@ -3,17 +3,14 @@ import React from 'react';
 
 const ImageCard = ({ image }) => {
   const { id, server, secret } = image;
-  //www.flickr.com/services/rest/?method=flickr.photos.getRecent&api_key=10e39868768af1480b8aa89c3efe73fa&format=json&nojsoncallback=1
-  https: return (
+
+  return (
     <GridItem
       rounded="md"
       display={'flex'}
       justifyContent={'center'}
       alignItems={'center'}
     >
-      {/* <Image
-        src={`https://live.staticflickr.com/${server}/${id}_${secret}.jpg`}
-      /> */}
       <Image
         src={`https://live.staticflickr.com/${server}/${id}_${secret}.jpg`}
       />
